97344bd7052fe47f7e66f9c88f48ada1819e2763
Daniel <demartinezgatell@gmail.com>
2020-01-06 14:54:36 +0000

    second test added

--- src/note-model.js
+++ src/note-model.js
@@ -13,16 +13,22 @@ var assert = {
 class Note {
   constructor() {
   }
-  write() {
+  write(input) {
+    this.input = input;
   }
   read() {
-    return 'It is a note';  
+    return this.input;
   }
 }
 
 
-//it can hold text
+//it returns it is note
 
 var testNote = new Note();
 testNote.write('It is a note');
 assert.isTrue(testNote.read(), 'It is a note');
+
+// it can return any given string
+var testNote2 = new Note();
+testNote2.write("I am very very calm I promise.")
+assert.isTrue(testNote2.read(), 'I am very very calm I promise.');
