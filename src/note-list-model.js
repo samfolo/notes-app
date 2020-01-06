a3cbaac8d44e1abc1d870fe6a7659594769e77c0
Daniel <demartinezgatell@gmail.com>
2020-01-06 16:07:26 +0000

    refactor

--- src/note-list-model.js
+++ src/note-list-model.js
@@ -14,7 +14,7 @@ class NoteList {
   }
   add(...notes) {
     notes.forEach(note => {
-      this.notes.push(note)
+      this.notes.push(new Note(note))
     });
   }
   showNotes(){
@@ -23,9 +23,9 @@ class NoteList {
 }
 
 var testNoteList = new NoteList();
-var testNote1 = new Note("Yehays");
-var testNote2 = new Note("Oh no");
-var testNote3 = new Note("Maybe");
-testNoteList.add(testNote1, testNote2, testNote3);
+// var testNote1 = new Note("Yehays");
+// var testNote2 = new Note("Oh no");
+// var testNote3 = new Note("Maybe");
+testNoteList.add("Yehays", "Oh no", "Maybe");
 var firstNote = testNoteList.showNotes()[0]
 assert.isTrue(firstNote, "Yehays");
